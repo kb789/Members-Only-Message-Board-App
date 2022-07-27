5bb9b6fe16e97ac20dbcf7f16abb7f0ec931250a
Kristina Bucher <bucher.kristina@gmail.com>
2022-07-27 13:35:06 -0400

    checking

--- app.js
+++ app.js
@@ -87,4 +87,4 @@ app.use((req, res, next) => {
   res.status(404).send('<h1>Page not found on the server</h1>');
 });
 
-app.listen(3000, () => console.log('app listening on port 3000!'));
+app.listen(process.env.PORT || 3000);
